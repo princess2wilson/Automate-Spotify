813d3e1e37de5da69e4e30baa5863f376b1ddbbb
Princess <wilson.princess@yahoo.ie>
2020-09-24 01:57:07 +0100

    created spotifyclient methods

---
+++ spotify_client.py
@@ -0,0 +1,13 @@
+import requests
+import urllib.parse
+
+
+class SpotifyClient(object):
+    def __init__(self, api_token):
+        self.api_token = api_token
+
+    def search_song(self, artist, track):
+        pass
+
+    def add_song_to_spotify(self, song_id):
+        pass
