863fc0b654bbd17adc7296c52b877f24a9f7599f
Princess <wilson.princess@yahoo.ie>
2020-09-24 18:29:31 +0100

    putting everythin together in main

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from youtube_client import YoutubeClient
 
 
 def main():
-    pass
+    #get playlists from our youtube
+    youtube_client=YoutubeClient('./creds/client_id.json')
+    playlists=youtube_client.get_playlist()
+
+    #ask which playlist we want the music video from, make it more interactive
+    for index,playlist in enumerate(playlists):
+        print(f"{index}:{playlist:title}")
+    choice =int(input("Enter your choice: "))
+
+
 if __name__=='__main__':
     main()
